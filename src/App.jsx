ea00e4b0ed3d96a8776ca4e63230b8f41d6ac51f
Nadav Cohen <nadav1999@gmail.com>
2022-10-18 20:25:09 +0300

    fixe gh-pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './assets/styles/main.scss';
 import { Home } from './pages/Home';
 
